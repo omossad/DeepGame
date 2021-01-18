7336df34ef746ded8aa0fc89745c40c1ecfc42ff
Omar <omossad@sfu.ca>
2021-01-18 08:52:05 -0800

    saliency map max and min

--- DeepLearning/ml.py
+++ DeepLearning/ml.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 path = 'C:\\Users\\omossad\\Desktop\\dataset\\raw_frames\\fifa\\ha_7\\frame_00249.jpg'
 image = cv2.imread(path)
 saliency = cv2.saliency.StaticSaliencySpectralResidual_create()
@@ -10,4 +11,6 @@ print(saliencyMap.shape)
 saliencyMap = (saliencyMap * 255).astype("uint8")
 cv2.imshow("Image", image)
 cv2.imshow("Output", saliencyMap)
+print(np.max(saliencyMap))
+print(np.min(saliencyMap))
 cv2.waitKey(0)
