45a6f0935e2d7b06a014c7e4ad5d1e0860e829a2
Omar <omossad@sfu.ca>
2021-01-18 08:46:41 -0800

    print saliency

--- DeepLearning/ml.py
+++ DeepLearning/ml.py
@@ -1,1 +1,6 @@
 import cv2
+path = 'C:\\Users\\omossad\\Desktop\\dataset\\raw_frames\\fifa\\ha_7\\frame_00249.jpg'
+image = cv2.imread(path)
+saliency = cv2.saliency.StaticSaliencyFineGrained_create()
+(success, saliencyMap) = saliency.computeSaliency(image)
+print(saliencyMap)
